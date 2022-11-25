345f8365fb6d05a5470f457cac4af9fe40d77edc
Kashika <kmohindru_be20@thapar.edu>
2022-11-25 13:40:20 +0530

    updated changes

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -10,10 +10,11 @@ const Hero = () => {
             <div className='flex flex-col justify-center md:items-start w-full px-2 pt-32 pb-8 mt-4'>
                 {/* <p className='text-2xl'>Unique Sequencing & Production</p> */}
                 <h1 className='py-3 text-5xl md:text-7xl font-bold text-fuchsia-200'>Digital Watermarking</h1>
-                <p className='text-2xl'>This is our Tech brand.</p>
-                <p className='text-2xl'>This is our Tech brand.</p>
-                <p className='text-2xl'>This is our Tech brand.</p>
-                
+                <div class='text-fuchsia-200 py-3'>
+                <p className='text-2xl'>Watermark/Decrypt pictures without waiting in line</p>
+                <p className='text-2xl'>Protect your business from unwanted</p>
+                <p className='text-2xl'>use of your images</p>
+                </div>
                 <Link to ="watermark" className='py-3 px-6 sm:w-[60%] my-4 text-2xl text-center cursor-pointer text-indigo-900 border bg-fuchsia-200 border-fuchsia-200 hover:bg-transparent hover:text-fuchsia-200 rounded-md'>Watermark</Link>
                 <Link to="extract" className='py-3 px-6 sm:w-[60%] mb-4 text-2xl text-center cursor-pointer text-indigo-900 border bg-fuchsia-200 border-fuchsia-200 hover:bg-transparent hover:text-fuchsia-200 rounded-md '>Decryption</Link>     
             </div>
